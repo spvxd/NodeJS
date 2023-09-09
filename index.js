88edc40cc9fd6d8309b0189d378a540149d2a565
spvxd <osipov27032002@mail.ru>
2023-09-09 13:11:08 +0400

    add handlebars

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const path = require('path')
-
+const exphbs  = require('express-handlebars');
 
 const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-    res.status(200) 
+   res.render('index')
 
 }) 
 
